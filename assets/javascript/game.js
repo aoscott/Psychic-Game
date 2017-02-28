1ee4b6112337d9e92603039c04f6c479dc2a53a4
Andy Scott <andy@aoscott.com>
2017-02-27 21:47:40 -0800

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,13 @@
 var userGuess
-var computerGuess
+var possible = "abcdefghijklmnopqrstuvwxyz";
+var computerGuess = possible.charAt(Math.floor(Math.random() * possible.length))
+var wins = 0
+var loss = 0
+var guessLeft = 5
+
+document.onkeydown =function(event) {
+	console.log(event.key);
+}
+
+
+console.log(computerGuess)
